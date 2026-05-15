3c6e5fd3037152cf337161f2b608ab0ba2b62f56
VivienEnderlin <djiomounandavivienenderlin@gmail.com>
2026-05-15 16:26:18 +0100

    commit test 7

--- api-commande/config/config.php
+++ api-commande/config/config.php
@@ -1,9 +1,14 @@
 <?php
-// config/jwt.php
+
+require __DIR__ . '/../../vendor/autoload.php';
+
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__ . '/../../');
+$dotenv->load();
+
 return [
-    'JWT_SECRET' => '9c3d8f41b8d4e6d1a7a0e9f93c6e3c5c8d7f4b0f5e8a1c9b0d6f7a8c9e1b2d3',
-    'JWT_ALGO'   => 'HS256',
-    'JWT_EXPIRE' => 86400 // 24h
+    'JWT_SECRET' => $_ENV['JWT_SECRET'],
+    'JWT_ALGO'   => $_ENV['JWT_ALGO'],
+    'JWT_EXPIRE' => $_ENV['JWT_EXPIRE']
 ];
 
 
