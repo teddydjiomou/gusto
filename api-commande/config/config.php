d2ca92103da82405f92f03ad7513bf7a15c91a2c
VivienEnderlin <djiomounandavivienenderlin@gmail.com>
2026-06-20 11:31:53 +0100

    version stable 70

--- api-commande/config/config.php
+++ api-commande/config/config.php
@@ -7,18 +7,18 @@ if (file_exists(__DIR__ . '/../../.env')) {
     $dotenv->load();
 }
 
-return [
-    'JWT_SECRET' => getenv('JWT_SECRET'),
-    'JWT_ALGO'   => getenv('JWT_ALGO'),
-    'JWT_EXPIRE' => getenv('JWT_EXPIRE')
-];
+// return [
+//     'JWT_SECRET' => getenv('JWT_SECRET'),
+//     'JWT_ALGO'   => getenv('JWT_ALGO'),
+//     'JWT_EXPIRE' => getenv('JWT_EXPIRE')
+// ];
 
 // ce qui marche en local
 
-// return [
-//     'JWT_SECRET' => $_ENV['JWT_SECRET'],
-//     'JWT_ALGO'   => $_ENV['JWT_ALGO'],
-//     'JWT_EXPIRE' => $_ENV['JWT_EXPIRE']
-// ];
+return [
+    'JWT_SECRET' => $_ENV['JWT_SECRET'],
+    'JWT_ALGO'   => $_ENV['JWT_ALGO'],
+    'JWT_EXPIRE' => $_ENV['JWT_EXPIRE']
+];
 
 //echo bin2hex(random_bytes(64)); pour genrer la clé secrete
